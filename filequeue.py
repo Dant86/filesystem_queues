c4b002299511c0ba7d5c70c350fdf1bdb5ba8c26
Dant86 <vedantdpathak@iCloud.com>
2017-09-20 23:37:31 -0500

    Completed task, yet to flake8

--- filequeue.py
+++ filequeue.py
@@ -1,126 +1,41 @@
 import os
 import sys
+from ll import LinkedList
 
-class Node:
+class Queue():
 
-	def __init__(self, value):
-		self.value = value
-		self.next = None
+    def __init__(self):
+        self.values = LinkedList()
 
-class LinkedList:
+    def add(self, newVal):
+        self.values.append(newVal)
 
-	def __init__(self):
-		self.head = None
-		self.tail = None
-		self.length = 0
+    def remove(self):
+        self.values.remove_from_front()
 
-	def append(self, i):
-		if self.head is None or self.tail is None:
-			self.head = Node(i)
-			self.tail = Node(i)
-			self.length += 1
-			return
-		newNode = Node(i)
-		self.tail.next = newNode
-		self.tail = newNode
-		self.length += 1
+    def length(self):
+        return self.values.length()
 
-	def prepend(self, i):
-		newNode = Node(i)
-		if self.head == None:
-			self.tail = newNode
-		newNode.next = self.head
-		self.head = newNode
-		self.length += 1
+    def itemAt(self, index):
+        return self.values.itemAt(index)
 
-	def removeFromFront(self):
-		newNode = self.head.next
-		self.head = newNode
-		self.length -= 1
+    def indexOf(self, value):
+        return self.values.indexOf(value)
 
-	def removeFromBack(self):
-		newNode = self.head
-		while(newNode.next.next is not None):
-			newNode = newNode.next
-		newNode.next = None
-		self.length -= 1
+    def __str__(self):
+        return str(self.values)
 
-	def itemAt(self, idx):
-		if idx > self.length-1 or idx < 0:
-			raise ValueError("Index out of bounds")
-		newNode = self.head
-		for i in range(self.length-1):
-			newNode = newNode.next
-		return newNode.value
+def get_files(path, queue):
+    for fname in os.listdir(path):
+        if "." in fname:
+            queue.add(fname)
+        else:
+            get_files(path + "/" + fname, queue)
 
-	def indexOf(self, i):
-		newNode = self.head
-		idx = 0
-		while(newNode is not None):
-			if newNode.value == i:
-				return idx
-			idx += 1
-		return -1
-
-	def set(self, newItem, idx):
-		if idx > self.length-1 or idx < 0:
-			raise ValueError("Index out of bounds")
-		newNode = self.head
-		for i in range(idx):
-			newNode = newNode.next
-		newNode.value = newItem
-
-	def add(self, newItem, idx):
-		newNode= Node(newItem)
-		if self.head is None:
-			self.head = newNode
-			self.tail = newNode
-			self.length += 1
-			return
-		if idx < 0:
-			raise ValueError("Index out of bounds")
-		if idx > self.length-1:
-			for i in range(idx-self.length):
-				self.append(0)
-		trav = self.head
-		for i in range(idx-1):
-			trav = trav.next
-		newNode.next = trav.next
-		trav.next = newNode
-		self.length += 1
-
-	def remove(self, idx):
-		if self.length == 1 and idx == 0:
-			self.head = None
-			self.tail = None
-			self.length -= 1
-			return
-		trav = self.head
-		for i in range(idx-1):
-			trav = trav.next
-		trav.next = trav.next.next
-
-	def isEmpty(self):
-		return this.head is None or this.tail is None
-
-	def __str__(self):
-		temp = self.head
-		retVal = "["
-		while temp is not None:
-			retVal += str(temp.value)
-			if temp.next is not None:
-				retVal += ", "
-			temp = temp.next
-		retVal += "]"
-		return retVal
-
-foo = LinkedList()
-foo.prepend(5)
-foo.append(578)
-foo.add(222, 1)
-foo.add(23468,3)
-foo.set(52, 3)
-foo.prepend(12)
-foo.append(123)
-print(foo)
-
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise ValueError("Usage: python filequeue.py [path_name]")
+    path = sys.argv[1]
+    files = Queue()
+    get_files(path, files)
+    print(files)
---
+++ ll.py
@@ -0,0 +1,120 @@
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, newVal):
+        newVal = Node(newVal)
+        if self.head is None or self.tail is None:
+            self.head = newVal
+            self.tail = newVal
+            self.length += 1
+            return
+        self.tail.next = newVal
+        self.tail = newVal
+        self.length += 1
+
+    def prepend(self, newVal):
+        newVal = Node(newVal)
+        if self.head is None or self.tail is None:
+            self.head = newVal
+            self.tail = newVal
+            self.length += 1
+            return
+        newVal.next = self.head
+        self.head = newVal
+        self.length += 1
+
+    def add(self, newVal, index):
+        newNode = Node(newVal)
+        if index < 0:
+            raise ValueError("Index < 0")
+        elif index == 0:
+            self.prepend(newVal)
+        elif index > 0 and index < self.length:
+            temp = self.head
+            for i in range(index - 1):
+                temp = temp.next
+            newNode.next = temp.next
+            temp.next = newNode
+            self.length += 1
+        elif index == self.length:
+            self.append(newVal)
+        else:
+            for i in range(index - self.length):
+                self.append(0)
+            self.append(newVal)
+
+    def set(self, newVal, index):
+        temp = self.head
+        for i in range(index):
+            temp = temp.next
+        temp.value = newVal
+
+    def remove_from_back(self):
+        temp = self.head
+        while temp.next.next is not None:
+            temp = temp.next
+        temp.next = None
+        self.length -= 1
+
+    def remove_from_front(self):
+        self.head = self.head.next
+        self.length -= 1
+
+    def remove(self, index):
+        if index < 0:
+            raise ValueError("Index < 0")
+        elif index == 0:
+            self.remove_from_front()
+        elif index > 0 and index < self.length - 1:
+            temp = self.head
+            for i in range(index - 1):
+                temp = temp.next
+            temp.next = temp.next.next
+            self.length -= 1
+        elif index == self.length - 1:
+            self.remove_from_back()
+        else:
+            raise ValueError("Index >= length of list")
+
+    def itemAt(self, index):
+        if index < 0:
+            raise ValueError("Index < 0")
+        elif index >= 0 and index < self.length:
+            temp = self.head
+            for i in range(index - 1):
+                temp = temp.next
+            return temp.value
+        elif index >= self.length:
+            raise ValueError("Index >= length of list")
+
+    def indexOf(self, value):
+        temp = self.head
+        index = 0
+        while temp is not None:
+            if temp.value == value:
+                return idx
+            idx += 1
+        return -1
+
+    def length():
+        return self.length()
+
+    def __str__(self):
+        temp = self.head
+        strVal = "["
+        while temp is not None:
+            strVal += str(temp.value)
+            if temp.next is not None:
+                strVal += ", "
+            temp = temp.next
+        return strVal + "]"
